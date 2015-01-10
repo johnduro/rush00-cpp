3a844002a3f468374bcfaf1aa3fc90d7a02beb14
Martin LE-ROY <mle-roy@e2r2p11.42.fr>
2015-01-10 18:23:47 +0100

    manager en route

--- gameManager.class.cpp
+++ gameManager.class.cpp
@@ -6,6 +6,128 @@
 //   By: mle-roy <mle-roy@student.42.fr>            +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2015/01/10 16:05:31 by mle-roy           #+#    #+#             //
-//   Updated: 2015/01/10 16:05:41 by mle-roy          ###   ########.fr       //
+//   Updated: 2015/01/10 18:16:27 by mle-roy          ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
+
+#include "gameManager.class.hpp"
+
+//refresh() --> raffraichi l ecran
+//int move(int y, int x); --> bouge le curseur
+
+
+// ** CANONICAL ** //
+gameManager::gameManager( void )
+{
+	this->_isInit = false;
+	this->_entities = new t_list;
+	this->_entities->start = NULL;
+	this->_entities->end = NULL;
+	this->_player = new Player();
+}
+
+gameManager::gameManager( gameManager const & src )
+{
+	//A FAIRE !!!!!!!
+}
+
+gameManager::~gameManager( void )
+{
+	if (this->isInit)
+		endwin(); //fin ncurses
+	delete this->_player;
+	this->_deleteAllEntities();
+}
+
+gameManager &			gameManager::operator=( gameManager const & src )
+{
+	//A FAIRE !!!!!!!
+}
+
+// ** PRIVATE FUNCTION ** //
+void					gameManager::_refresh( void )
+{
+	wrefresh(this->_field);
+	wrefresh(this->_score);
+}
+
+void					gameManager::_drawBorder(WINDOW *screen)
+{
+	int		x;
+	int		y;
+	int		i;
+
+	getmaxyx(screen, y, x);
+	// corners
+	mvwprintw(screen, 0, 0, "+");
+	mvwprintw(screen, y - 1, 0, "+");
+	mvwprintw(screen, 0, x - 1, "+");
+	mvwprintw(screen, y - 1, x - 1, "+");
+	// sides
+	for (i = 1; i < (y - 1); i++)
+	{
+		mvwprintw(screen, i, 0, "|");
+		mvwprintw(screen, i, x - 1, "|");
+	}
+	// top and bottom
+	for (i = 1; i < (x - 1); i++)
+	{
+		mvwprintw(screen, 0, i, "-");
+		mvwprintw(screen, y - 1, i, "-");
+	}
+}
+
+void					gameManager::_deleteAllEntities( void )
+{
+	t_entity	*ptr;
+	t_entity	*keep;
+
+	ptr = this->_entities->start;
+	while (ptr)
+	{
+		keep = ptr->next;
+		delete ptr->entity;
+		delete ptr;
+		ptr = keep;
+	}
+	delete this->_entities;
+}
+
+void					gameManager::_addEntity(gameEntity* newEntity)
+{
+	t_entity		*node = new t_entity;
+
+	node->entity = newEntity;
+	node->next = NULL;
+	node->prev = NULL;
+	node->prev = this->_entities->end;
+	this->_entities->end->next = node;
+}
+
+// ** PUBLIC FUNCTION **//
+void					gameManager::init( void )
+{
+	//initialisation ncurses
+	initscr();
+	clear();
+	noecho();
+	cbreak();
+	keypad(stdscr, TRUE);
+	curs_set(0);
+	timeout(0);
+	getmaxyx(stdscr, this->_maxY, this->_maxX);
+	this->_field = newwin(this->_maxY - this->_scoreSize, this->_maxX, 0, 0);
+	this->_score = newwin(this->_scoreSize, this->_maxX, this->_maxY - this->_scoreSize, 0);
+	mvwprintw(this->_field, 0, 0, "Field");
+	mvwprintw(this->_score, 0, 0, "Score");
+	this->_refresh();
+	this->_isInit = true;
+}
+
+void					gameManager::loop( void )
+{
+	while (42)
+	{
+
+	}
+}
--- gameManager.class.hpp
+++ gameManager.class.hpp
@@ -6,21 +6,61 @@
 //   By: mle-roy <mle-roy@student.42.fr>            +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2015/01/10 16:05:24 by mle-roy           #+#    #+#             //
-//   Updated: 2015/01/10 16:11:17 by mle-roy          ###   ########.fr       //
+//   Updated: 2015/01/10 18:13:46 by mle-roy          ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
 #ifndef GAMEMANAGER_CLASS_HPP
 # define GAMEMANAGER_CLASS_HPP
 
+#include <ncurses.h>
+#include "gameEntity.class.hpp"
+
+typedef struct			s_entity
+{
+	gameEntity*			entity;
+	struct s_entity		*next;
+	struct s_entity		*prev;
+}						t_entity;
+
+typedef struct			s_list
+{
+	t_entity			*start;
+	t_entity			*end;
+}						t_list;
+
+
+// ** CLASS GAME MANAGER** //
+
 class gameManager
 {
+private :
+	// ** PRIVATE ATTRIBUTES ** //
+	static int const	_scoreSize = 3;
+	bool				_isInit;
+	t_list				*_entities;
+	Player				*_player;
+	int					_maxY;
+	int					_maxX;
+	WINDOW				*_field;
+	WINDOW				*_score;
+
+	// ** PRIVATE FUNCTION ** //
+	void				_refresh( void );
+	void				_drawBorders(WINDOW *screen);
+	void				_deleteAllEntities( void );
+	void				_addEntity(gameEntity* newEntity);
+
 public :
 	// ** CANONICAL ** //
 	gameManager( void );
 	gameManager( gameManager const & src );
 	~gameManager( void );
 	gameManager &			operator=( gameManager const & src );
-}
+
+	// ** PUBLIC FUNCTION ** //
+	void					init( void );
+	void					loop( void );
+};
 
 #endif // ** GAMEMANAGER_CLASS_HPP ** //
